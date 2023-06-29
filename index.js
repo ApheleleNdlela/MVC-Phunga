8e4b83d31b5ec9edc448d75d57a6115498770e37
Aphelele Ndlela <aphelelendlela95@gmail.com>
2023-06-29 14:53:12 +0200

    new updates

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ db.mongoose.connect(DB.db_url, {
 
 // initRoutes(app);
 require("./routes/item.routes")(app)
-require("../Phunga-App/routes/auth.routes")(app);
-require("../Phunga-App/routes/user.routes")(app);
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}.`);
