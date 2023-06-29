96484074b3e039ca3795c3cb41a7eca827354622
Aphelele Ndlela <aphelelendlela95@gmail.com>
2023-06-29 14:52:34 +0200

    Updates

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
 module.exports = {
-    db_url: "mongodb://127.0.0.1:27017/phunga_db" 
+    // db_url: "mongodb://127.0.0.1:27017/phunga_db"
+    db_url: process.env.phunga_db
 };
